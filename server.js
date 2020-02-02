6e2681a1721cc288b3538bae182b1bb65681f88b
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-02-02 17:13:44 -0300

    removendo http como parametro

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const server = require('http').createServer('http')
+const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
